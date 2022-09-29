bf39d0ca118ffc2be05a7d2cae3585a48737bc96
Felipe Rosas <felipe.rosas@g.globo>
2022-09-29 09:45:37 -0300

    chore: add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./styles/theme";
+
 function App() {
-  return <h1>Makkari app</h1>;
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <h1>Makkari app</h1>
+    </ChakraProvider>
+  );
 }
 
 export default App;
